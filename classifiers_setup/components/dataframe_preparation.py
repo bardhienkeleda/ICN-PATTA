8d79503c9f63e7777ba9702d9ae7fefe47307f57
bardhienkeleda <enkeleda.bardhi@studenti.unipd.it>
2021-09-24 18:05:39 +0200

    updated components

--- classifiers_setup/components/dataframe_preparation.py
+++ classifiers_setup/components/dataframe_preparation.py
@@ -14,6 +14,8 @@ import string
 from sklearn.feature_selection import chi2
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import preprocessing
+from sklearn.utils import shuffle
+import wordninja
 
 """
 This class preprocess the dataset by filling some missing values, dropping NaN labeled rows,
@@ -23,7 +25,7 @@ Furthermore, it calculates TF-IDF features.
 class Preprocessing():
 
     def __init__(self):
-        self.domain_path = abspath(dirname(__file__)  + '/../domain')
+        self.domain_path = abspath(dirname(__file__)  + '/../domain/')
         self.tfidf_vectors_path = (abspath(dirname(__file__)) + "/saved_tfidf_vectors")
 
     def preprocessing(self, df):
@@ -32,65 +34,72 @@ class Preprocessing():
         It returns the preprocessed dataframe.
         """
         not_to_keep = []
-
+        """
         for webpg in range (len(configuration.webpages_list)):
             div = re.sub('[^A-Za-z0-9]+', ' ', str(configuration.webpages_list[webpg]))
             not_to_keep.append(div)
         for webpg in range (len(not_to_keep)):
             not_to_keep[webpg] = not_to_keep[webpg].split(" ")
 
-
+        """
         # prepare a list of words that should be droped
-        words_to_delete = [item for sublist in not_to_keep for item in sublist]
-        words_to_delete.append('http')
-        words_to_delete.append('wwwf')
-        words_to_delete.append('co') #
-        words_to_delete.append('php')
-        words_to_delete.append('html')
-        words_to_delete.append('cn') #
-        words_to_delete.append('www')
+        #words_to_delete = [item for sublist in not_to_keep for item in sublist]
+        not_to_keep.append('http')
+        not_to_keep.append('https')
+        not_to_keep.append('ftp')
+        not_to_keep.append('www')
+        not_to_keep.append('wwwf')
+        not_to_keep.append('www2')
+        not_to_keep.append('www3')
+        not_to_keep.append('co') #
+        not_to_keep.append('php')
+        not_to_keep.append('html')
+        not_to_keep.append('cn') #
 
-        words_to_delete = list(set(words_to_delete))
 
-        dataframe = df.copy()
+        #words_to_delete = list(set(words_to_delete))
+
+        #dataframe = pd.DataFrame(columns = df.columns, index= df.index)
+        #dataframe = df.copy()
         #print(dataframe.head(5))
         # delete all the special characters from each url
         for row in range (0, len(df)):
+            print(row)
             word_pattern = re.compile('[^A-Za-z0-9]+')
-            dataframe['Full request URI'][row] = word_pattern.sub(' ', str(df['Full request URI'][row]))
-            res = dataframe['Full request URI'][row].split()
-            #print("Res: {}".format(res))
-            #results = []
-            """
-            for word in res:
-                if (len(word) > 3) & (word.isalpha()):
-                    print(word)
-                else:
-                    res.remove(word)
-            #print("Result: {}".format(res))
-            dataframe['Full request URI'][row] = ' '.join(res)
-            #print("Dataframe prima \n")
-            print("Dataframe row: {}".format(dataframe['Full request URI'][row]))
-            """
+            #prep =
+            #dataframe = dataframe.append(word_pattern.sub(' ', str(df['Full request URI'][row])))
+            df['Full request URI'][row] = word_pattern.sub(' ', str(df['Full request URI'][row]))
+            #dataframe.to_csv("prep_df.csv")
 
-            for index, word in enumerate(res):
-                if (len(word) <= 3) or (word.isalpha == False) or (word in words_to_delete):
-                #if word in not_to_keep:
+            res = df['Full request URI'][row].split()
+            #print("Res: {}".format(res))
+            results = []
+            for word in res:
+                #print("Printing word {}".format(word))
+                word_list = wordninja.split(word)
+                #print("Printing word list {}".format(word_list))
+                results.append(word_list)
+            flat_results = [item for sublist in results for item in sublist]
+            #print("Results: {}".format(flat_results))
+
+            for index, word in enumerate(flat_results):
+                if (len(word) <= 3) or (word.isalpha == False) or (word in not_to_keep):
+                    #print("Word is {}".format(word))
                     if index == 0:
-                        dataframe['Full request URI'][row] = dataframe['Full request URI'][row].replace(word + ' ', ' ')
+                        df['Full request URI'][row] = df['Full request URI'][row].replace(word + ' ', ' ')
                         #print(value)
                         #print(dataframe['Full request URI'][row])
                     elif index == (len(res) - 1):
-                        dataframe['Full request URI'][row] = dataframe['Full request URI'][row].replace(' ' + word , ' ')
+                        df['Full request URI'][row] = df['Full request URI'][row].replace(' ' + word , ' ')
                     else:
-                        dataframe['Full request URI'][row] = dataframe['Full request URI'][row].replace(' ' + word + ' ', ' ')
-        #print("Dataframe dopo\n")
+                        df['Full request URI'][row] = df['Full request URI'][row].replace(' ' + word + ' ', ' ')
+                #print(dataframe['Full request URI'][row])
         #print(dataframe.head(10))
         #df.drop(['Full request URI', 'Full request URI prep'], axis=1, inplace = True)
         #dataframe.drop("index", axis=1, inplace=True)
-        dataframe.columns = ['Full_NDN_interest', 'Label']
+        df.columns = ['Full_NDN_interest', 'Label']
 
-        return dataframe
+        return df
 
 
     def preparation(self):
@@ -100,12 +109,34 @@ class Preprocessing():
         It returns the final dataframe.
         """
 
-        df = pd.read_csv(self.domain_path + '/' + 'categorized_dataset.csv')
+        df = pd.read_csv(self.domain_path + '/updated_dataset.csv')
         df.drop("Unnamed: 0", axis=1, inplace=True)
-        #df.drop("index", axis=1, inplace=True)
-        df = df.reset_index(drop=True)
+        print(df)
+        """
+        df = shuffle(df)
+        df = df.reset_index()
+        df_truncated = df.iloc[:50000]
+        df_truncated.drop("index", axis=1, inplace=True)
+        df_truncated = df_truncated.reset_index(drop=True)
+        print(df_truncated)
+
+        df_train_truncated = df_truncated.iloc[:35000]
+        df_train_truncated = df_train_truncated.reset_index(drop=True)
+        print(df_train_truncated)
+
+        df_test_truncated = df_truncated.iloc[35000:]
+        df_test_truncated = df_test_truncated.reset_index(drop=True)
+        print(df_test_truncated)
+
+        # write train and test dataframes into files for later use
+        with open(self.domain_path + "/new_dataset/" + "truncated_train_instances.csv" , "w") as file:
+            df_train_truncated.to_csv(file)
+        with open(self.domain_path + "/new_dataset/" + "truncated_test_instances.csv" , "w") as file:
+            df_test_truncated.to_csv(file)
+        """
         # call preprocessing method and preprocess dataframe
         #df = df.reset_index(drop=True)
+
         df = self.preprocessing(df)
 
         # encode the labels
@@ -128,20 +159,22 @@ class Preprocessing():
         """
         print("Calculating tfidf...")
         # load train and test instances into dataframes
-        train_df = pd.read_csv(self.domain_path + "/" + "train_instances_new.csv")
+        train_df = pd.read_csv(self.domain_path + "/train_instances_new.csv")
+        print("Train df read...")
+        print(train_df)
         train_df.drop("Unnamed: 0", axis=1, inplace=True)
         #train_df.drop("index", axis=1, inplace=True)
         train_df = self.preprocessing(train_df)
         train_df['Category_ID'] = self.label_encoder.transform(train_df['Label'].tolist())
 
-        test_df = pd.read_csv(self.domain_path + "/" + "test_instances_new.csv")
+        test_df = pd.read_csv(self.domain_path + "/test_instances_new.csv")
         test_df.drop("Unnamed: 0", axis=1, inplace=True)
         #test_df.drop("index", axis=1, inplace=True)
         test_df = self.preprocessing(test_df)
         test_df['Category_ID'] = self.label_encoder.transform(test_df['Label'].tolist())
 
         # calculate tfidf for both train and test
-        self.tfidf = TfidfVectorizer(sublinear_tf=True, min_df =10, ngram_range=configuration.GRAMS, stop_words='english') #min_df =5, max_features= configuration.NUMBER_FEATURES,
+        self.tfidf = TfidfVectorizer(sublinear_tf=True, min_df = 5, max_features= configuration.NUMBER_FEATURES, ngram_range=configuration.GRAMS, stop_words='english') #min_df =5, max_features= configuration.NUMBER_FEATURES,
         self.train_features = self.tfidf.fit_transform(train_df.Full_NDN_interest).toarray()
         #print(self.train_features)
         self.test_features = self.tfidf.transform(test_df.Full_NDN_interest).toarray()
@@ -151,11 +184,11 @@ class Preprocessing():
 
         # save tfidf test vectors which will be used for online analysis
         if configuration.WRITE_MODELS:
-            with open(self.tfidf_vectors_path + "/" + "tfidf_vector_1gram_653feat.pk" , "wb") as file:
+            with open(self.tfidf_vectors_path + "/" + "tfidf_vector_1gram_xfeat.pk" , "wb") as file:
             	pickle.dump(self.tfidf, file)
-            with open(self.tfidf_vectors_path + "/" + "train_features_1gram_653feat.pk" , "wb") as file:
+            with open(self.tfidf_vectors_path + "/" + "train_features_1gram_xfeat.pk" , "wb") as file:
             	pickle.dump(self.train_features, file)
-            with open(self.tfidf_vectors_path + "/" + "train_labels_1gram_653feat.pk" , "wb") as file:
+            with open(self.tfidf_vectors_path + "/" + "train_labels_1gram_xfeat.pk" , "wb") as file:
             	pickle.dump(self.train_labels, file)
 
         print("Each of the %d training NDN interest names is represented by %d features" % (self.train_features.shape))
